ce65b809207a91da4db6296fc805841f38bf3817
Nidhi8595 <155657850+Nidhi8595@users.noreply.github.com>
2024-10-03 23:29:15 +0530

    Undrrstanding basics of React

---
+++ Basics/App.js
@@ -0,0 +1,33 @@
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+const heading = React.createElement("h1", { id: "Heading" }, "Hello World from React!");
+
+root.render(heading);
+
+
+const root2 = ReactDOM.createRoot(document.querySelector("#root2"));
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+
+    React.createElement(
+        "div",
+        { id: "child" },
+
+        [React.createElement(
+            "h1",
+            {},
+
+            "A h1 tag"
+        ),
+        React.createElement(
+            "h2",
+            {},
+
+            "A h2 tag"
+        )]
+    )
+);
+
+root2.render(parent);

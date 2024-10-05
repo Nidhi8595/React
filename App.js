2bcf8a71366dc70c5a9e58933beb55b0029b676e
Nidhi8595 <155657850+Nidhi8595@users.noreply.github.com>
2024-10-05 14:33:29 +0530

    ...

--- App.js
+++ App.js
@@ -5,14 +5,15 @@ const Header = () => {
     return (
         <div className="header">
             <div className="logo-container">
-            <img className="logo" src="/logo.png" alt="Logo" />
+                <img className="logo" src="https://www.canva.com/design/DAGSsC-RlxI/I7W4z-Bdfa7nfRZxUWDvKg/view?utm_content=DAGSsC-RlxI&utm_campaign=share_your_design&utm_medium=link&utm_source=shareyourdesignpanel" alt="Logo" />
+
             </div>
 
             <div className="nav-items">
                 <ul>
                     <li>Home</li>
                     <li>About Us</li>
-                    <li>contact Us</li>
+                    <li>Contact Us</li>
                     <li>Cart</li>
                 </ul>
             </div>

d75a4152c0379a92ada7a063927c379f3d3a9da8
Nidhi8595 <155657850+Nidhi8595@users.noreply.github.com>
2024-11-22 23:03:29 +0530

    Updates

---
+++ Grocery.ea2e958b.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire94c2;(0,e.register)("loR3J",function(r,t){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>i,set:void 0,enumerable:!0,configurable:!0});var o=e("ayMG0"),i=()=>/*#__PURE__*/(0,o.jsx)("h1",{style:{textAlign:"center",marginTop:"100px"},children:"Our Grocery Online Store, we have a lot of child components inside this web page"})});
+//# sourceMappingURL=Grocery.ea2e958b.js.map
